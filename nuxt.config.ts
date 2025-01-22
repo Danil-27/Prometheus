1844ea69c211ecd47eb28851504fa3a475627cf5
Danil-27 <pde200126@gmail.com>
2025-01-22 19:10:39 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        module: 'ESNext',
+      },
+    },
+  },
+  eslint: {
+    /* module options */
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    'nuxt-viewport',
+    '@nuxtjs/i18n',
+    '@nuxt/image',
+    '@nuxt/eslint',
+  ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div>3322</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
