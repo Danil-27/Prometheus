fd82633221d46b043518dd6ae2b5456cb5dbbfcf
Danil-27 <pde200126@gmail.com>
2025-05-04 16:24:46 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,12 +41,80 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
 
     head: {
-      title: 'Мое приложение',
+      title: 'Прометей',
       meta: [
-        { name: 'description', content: 'Основное описание моего приложения' },
-        { property: 'og:title', content: 'Заголовок для Open Graph' }
+        {
+          name: 'description',
+          content:
+            'Прометей — студия интерьерного дизайна с 14-летним опытом. Полный спектр услуг: от концепции до реализации с 3D-визуализацией и надзором.'
+        },
+
+        // Open Graph
+        {
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'Прометей'
+        },
+        {
+          property: 'og:title',
+          content:
+            'Полный спектр услуг по дизайну интерьера: от концепции до реализации.'
+        },
+        {
+          property: 'og:description',
+          content:
+            'Прометей — студия интерьерного дизайна с 14-летним опытом. Полный спектр услуг: от концепции до реализации с 3D-визуализацией и надзором.'
+        },
+
+        // {
+        //   property: 'og:url',
+        //   content: 'http://localhost:3000/'
+        // },
+        {
+          property: 'og:locale',
+          content: 'ru_RU'
+        },
+        {
+          property: 'og:image',
+          content:
+            'https://i0.wp.com/www.escapereview.be/wp-content/uploads/2022/01/2.jpg?fit=1200%2C630&ssl=1'
+        },
+        {
+          property: 'og:image:width',
+          content: '968'
+        },
+        {
+          property: 'og:image:height',
+          content: '504'
+        }
       ],
-      link: [{ rel: 'icon', href: '/favicon.ico' }]
+      link: [
+        { rel: 'icon', href: '/favicon/favicon.ico' },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon/favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon/favicon-16x16.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/favicon/apple-touch-icon.png'
+        },
+        {
+          rel: 'manifest',
+          href: '/favicon/site.webmanifest'
+        }
+      ]
     }
   },
   i18n: {
