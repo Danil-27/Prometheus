df5fb1f8be1a1e23843eab60172e4c77080299f6
Danil-27 <pde200126@gmail.com>
2025-05-17 21:10:41 +0300

    CI-CD

--- components/header/HeaderDesctop.vue
+++ components/header/HeaderDesctop.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="mx-auto custom-container">
-    <div
-      class="flex justify-between bg-white items-center h-[5rem] xxl:h-[6.25rem] z-10 px-[50px] xl:px-[60px] xxl:px-[88px ] rounded-b-base"
-    >
+    <div class="flex justify-between bg-white items-center h-[5rem] xxl:h-[6.25rem] z-10 px-[50px] xl:px-[60px] xxl:px-[88px ] rounded-b-base">
       <Logo class="logo" />
+
       <div class="flex">
         <nav>
           <ul class="flex justify-center items-center gap-6">
             <li v-for="link in linksDesctop" :key="link.id">
-              <nuxt-link
-                class="text-black p-[5px] lg:text-[18px] xxl:text-[24px]"
-                :class="{ 'bg-black text-white rounded-base px-[35px] py-[8px] ': link.id == 5 }"
-                :to="link.url"
-              >
+              <nuxt-link class="text-black p-[5px] lg:text-[18px] xxl:text-[24px]" :class="{ 'bg-black text-white rounded-base px-[35px] py-[8px] ': link.id == 5 }" :to="link.url">
                 {{ link.title }}
+                хуки наше все
               </nuxt-link>
             </li>
           </ul>
@@ -24,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-  import { linksDesctop } from './type';
+import { linksDesctop } from './type';
 </script>
 
 <style scoped lang="scss"></style>
