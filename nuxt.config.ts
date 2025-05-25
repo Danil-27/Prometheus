866c489901c99e47ccfc0fada20c7b58c8a3f230
Danil-27 <pde200126@gmail.com>
2025-05-25 21:36:49 +0300

    fix: seo nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -122,27 +122,27 @@ export default defineNuxtConfig({
       ],
 
       link: [
-        { rel: 'icon', href: '/favicon/favicon.ico' },
+        { rel: 'icon', href: '/favicon.ico' },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '32x32',
-          href: '/favicon/favicon-32x32.png'
+          href: '/favicon-32x32.png'
         },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '16x16',
-          href: '/favicon/favicon-16x16.png'
+          href: '/favicon-16x16.png'
         },
         {
           rel: 'apple-touch-icon',
           sizes: '180x180',
-          href: '/favicon/apple-touch-icon.png'
+          href: '/apple-touch-icon.png'
         },
         {
           rel: 'manifest',
-          href: '/favicon/site.webmanifest'
+          href: '/site.webmanifest'
         }
       ]
     }
