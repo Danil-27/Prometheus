18b58e09e60bb1549ab1affed57606dacd5d034b
Danil-27 <pde200126@gmail.com>
2025-06-15 20:58:13 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import svgLoader from 'vite-svg-loader';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2024-04-03',
 
   devtools: { enabled: false },
