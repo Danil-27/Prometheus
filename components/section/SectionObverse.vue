153ed32150fe7f1256e1e72f11b6b504a05ffde3
Danil-27 <pde200126@gmail.com>
2025-08-30 00:08:49 +0300

    Nuxt-images

--- components/section/SectionObverse.vue
+++ components/section/SectionObverse.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="custom-container mt-[20px]">
-    <div class="relative text-[white] rounded-base xl:bg-green min-h-[25rem] lg:min-h-[540px] xxl:min-h-[700px] 
-    bg-[url('~/assets/images/obverse/obverse-mini.png')] 
-    md:bg-[url('~/assets/images/obverse/obverse.jpg')]
-     
-     bg-cover bg-center bg-no-repeat">
-      <div class="absolute left-0 top-0 h-full w-full bg-[linear-gradient(90deg,rgba(0,0,0,0.69)_0%,rgba(255,255,255,0)_100%)] bg-[rgba(0,0,0,0.36)] px-[1rem] py-[3.75rem] md:px-[2rem] md:py-[2.75rem] lg:pl-[58px] lg:pt-[130px] xl:pb-[9.375rem] rounded-base">
+    <div class="relative text-white rounded-base min-h-[25rem] lg:min-h-[540px] xxl:min-h-[700px] overflow-hidden">
+
+      <!-- LCP-картинка -->
+      <NuxtImg src="/images/obverse/obverse.jpg" format="webp" quality="80" preload class="absolute inset-0 w-full h-full object-cover" alt="Прометей Несем свет, создаем эстетику, вдыхаем уют" />
+
+      <!-- затемняющий градиент -->
+      <div class="absolute inset-0 bg-[linear-gradient(90deg,rgba(0,0,0,0.69)_0%,rgba(255,255,255,0)_100%)] bg-[rgba(0,0,0,0.36)] px-[1rem] py-[3.75rem] md:px-[2rem] md:py-[2.75rem] lg:pl-[58px] lg:pt-[130px] xl:pb-[9.375rem] rounded-base">
+
         <p class="font-jetExtraB text-[2.5rem] xs:text-[4rem] sm:text-[5.6rem] xl:text-[120px] xxl:text-[10rem] leading-[1]">
           Прометей
         </p>
