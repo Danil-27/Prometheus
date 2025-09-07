19398daa53f096b51e8c56e20f8f16f7db52d821
Danil-27 <pde200126@gmail.com>
2025-09-07 03:59:31 +0300

    uninstall V-mask

--- plugins/v-mask.client.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtPlugin } from '#app';
-import VueTheMask from 'vue-the-mask';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  if (import.meta.client) {
-    nuxtApp.vueApp.use(VueTheMask);
-  }
-});
